0357c9b313082ce3cfff3f4e1af84e04e5ecbe24
BennabPS <BennabPS@FTL1860244C7E5E.cdhs.state.co.us>
2019-02-09 13:44:05 -0700

    Initial Commit of project.

---
+++ TestJavaProject/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main ( String[] args) {
+	
+		displayGreetings();
+	}
+
+	private static void displayGreetings() {
+		System.out.println("Hello Good Morning!");
+		System.out.println("-------------------");
+	}
+	
+}
+	
